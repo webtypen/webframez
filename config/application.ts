4695263eab08d2756d1b9c474b4c1de1ba2364ee
TorrnexT <benedict.romp@googlemail.com>
2022-11-25 13:46:51 +0100

    Create application.ts

---
+++ config/application.ts
@@ -0,0 +1,14 @@
+export default {
+    /**
+     * Configure the Router
+     */
+    router: {
+        requests: {
+            /**
+             * Define wether Requests should be logged in console:
+             * { date: "2022-11-25 09:04:12", ip: "127.0.0.1", path: "/api/dashboard/data" }
+             */
+            logConsole: true,
+        },
+    },
+};
