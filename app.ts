671c752809afaf43a62371882d81d0f82323b732
TorrnexT <benedict.romp@googlemail.com>
2022-11-25 13:48:15 +0100

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,8 +1,20 @@
+import dotenv from "dotenv";
 import { WebApplication } from "@webtypen/webframez-core";
-// import { DBDrivers } from "@webtypen/webframez-core/src/Database/DBDriver";
+// import { DBDrivers } from "@webtypen/webframez-core";
 // import { MongoDBDriver } from "@webtypen/webframez-dbdriver-mongodb";
+import { Kernel } from "./app/Kernel";
+import application from "./config/application";
+import database from "./config/database";
 
 // DBDrivers.register("mongodb", MongoDBDriver);
 
 const app = new WebApplication();
-app.boot();
+app.boot({
+    kernel: Kernel,
+    config: {
+        application: application,
+        database: database,
+    },
+    port: process.env.PORT ? process.env.PORT : 3000,
+    basename: process.env.BASENAME ? process.env.BASENAME : null,
+});
